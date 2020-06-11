c1f6a85accbdfa1333e89ae599e3ea7fe6c33705
Azenga Kevin <azenga.kevin7@gmail.com>
2020-06-11 14:54:50 +0300

    Updated the Dao

--- app/src/main/java/com/propscout/scnotesapp/db/NoteDao.kt
+++ app/src/main/java/com/propscout/scnotesapp/db/NoteDao.kt
@@ -1,18 +1,25 @@
 package com.propscout.scnotesapp.db
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface NoteDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addNote(note: Note)
 
     @Query("SELECT * FROM note ORDER BY id DESC")
     suspend fun getAllNotes(): List<Note>
 
+    @Query("SELECT * FROM note WHERE id = :id")
+    suspend fun getNoteById(id: Int)
+
     @Insert
     suspend fun addMultipleNotes(vararg note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteItem(note: Note)
 }
